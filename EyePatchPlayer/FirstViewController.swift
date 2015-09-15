8278345755dddc59b1a3af6c35e4eadffdf80fa9
Andr3y <tovdev@gmail.com>
2015-09-15 12:48:47 +0100

    Initial Commit

---
+++ EyePatchPlayer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EyePatchPlayer
+//
+//  Created by Andr3y on 15/09/2015.
+//  Copyright (c) 2015 Apppli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EyePatchPlayer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EyePatchPlayer
+//
+//  Created by Andr3y on 15/09/2015.
+//  Copyright (c) 2015 Apppli. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
