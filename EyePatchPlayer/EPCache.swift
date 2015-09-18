8dc67cb051477686e947ff890653de96df072266
Andr3y <tovdev@gmail.com>
2015-09-18 11:04:42 +0100

    caching attempt 1 (start)

---
+++ EyePatchPlayer/EPCache.swift
@@ -0,0 +1,13 @@
+//
+//  EPCache.swift
+//  EyePatchPlayer
+//
+//  Created by Andr3y on 18/09/2015.
+//  Copyright (c) 2015 Apppli. All rights reserved.
+//
+
+import UIKit
+
+class EPCache: NSObject {
+   
+}
