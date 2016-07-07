ee61d06235d80cd88f9372ddf5e3e890644997db
Andr3y <pryanik2k@gmail.com>
2016-07-07 18:16:57 +0100

    1.0.14 (3)

--- EyePatchPlayer/EPConstants.swift
+++ EyePatchPlayer/EPConstants.swift
@@ -10,19 +10,26 @@ import Foundation
 
 struct EPConstants{
     
+//#if VKAPPID
     struct VK {
-        static let AppID = ""
+        static let AppID = "5070798"
     }
+//#endif
     
+//#if PARSEAPPID
     struct Parse {
-        static let Key = ""
-        static let AppID = ""
+        static let Key = "ivW39UauexSmxw1jkBNnzJvGGPupTRtDq2KNSp8k"
+        static let AppID = "EiNXLsL2dqGqFTfeRrEhKjI0pVwzqiiDaktKKBaN"
     }
+//#endif
     
+//#if LASTFMAPIKEY
+
     struct LastFM {
-        static let APIKey = ""
-        static let Secret = ""
+        static let APIKey = "e0f77b7ccf49aad3aa10d16bb432a5ed"
+        static let Secret = "93ec99df32f162bcf58985fb975d6014"
         static let APIRootURL = "https://ws.audioscrobbler.com/2.0/"
     }
-    
+//#endif
+
 }
