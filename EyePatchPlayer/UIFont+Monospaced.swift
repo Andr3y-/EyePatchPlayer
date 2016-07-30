18ee99ad5ce55f393ca69a1c3a07b92632557352
Andr3y <pryanik2k@gmail.com>
2016-07-30 18:01:24 +0100

    1.0.17 (2) Fixed Release crash for monospaced fonts

--- EyePatchPlayer/UIFont+Monospaced.swift
+++ EyePatchPlayer/UIFont+Monospaced.swift
@@ -7,22 +7,12 @@
 //
 
 extension UIFont {
-    
     var monospacedDigitFont: UIFont {
-        let oldFontDescriptor = fontDescriptor()
-        let newFontDescriptor = oldFontDescriptor.monospacedDigitFontDescriptor
-        return UIFont(descriptor: newFontDescriptor, size: 0)
-    }
-    
-}
-
-private extension UIFontDescriptor {
-    
-    var monospacedDigitFontDescriptor: UIFontDescriptor {
         let fontDescriptorFeatureSettings = [[UIFontFeatureTypeIdentifierKey: kNumberSpacingType, UIFontFeatureSelectorIdentifierKey: kMonospacedNumbersSelector]]
         let fontDescriptorAttributes = [UIFontDescriptorFeatureSettingsAttribute: fontDescriptorFeatureSettings]
-        let fontDescriptor = self.fontDescriptorByAddingAttributes(fontDescriptorAttributes)
-        return fontDescriptor
+        let oldFontDescriptor = fontDescriptor()
+        let newFontDescriptor = oldFontDescriptor.fontDescriptorByAddingAttributes(fontDescriptorAttributes)
+        
+        return UIFont(descriptor: newFontDescriptor, size: 0)
     }
-    
 }
