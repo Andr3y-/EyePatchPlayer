b611f1e8074bc3f540de069bcf428d6e4da528af
Andrey <a.staroseltsev@quidco.com>
2017-04-28 14:03:49 +0100

    replaced Fabric/Crashlytics pods with frameworks

---
+++ Frameworks/RESideMenuWrapper/RESideMenuWrapper.h
@@ -0,0 +1,21 @@
+//
+//  RESideMenuWrapper.h
+//  RESideMenuWrapper
+//
+//  Created by Andrey Staroseltsev on 4/27/17.
+//  Copyright © 2017 Apppli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <RESideMenuWrapper/RESideMenu.h>
+
+//! Project version number for RESideMenuWrapper.
+FOUNDATION_EXPORT double RESideMenuWrapperVersionNumber;
+
+//! Project version string for RESideMenuWrapper.
+FOUNDATION_EXPORT const unsigned char RESideMenuWrapperVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <RESideMenuWrapper/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -9,8 +9,6 @@ pod 'VK-ios-sdk', '~>1.2.2'
 pod 'Realm'
 pod 'SDWebImage', '~>3.7'
 pod 'StreamingKit', '~>0.1.29'
-pod 'Fabric', '~> 1.6.3'
-pod 'Crashlytics', '~> 3.6.0'
 pod 'DGActivityIndicatorView', '~> 2.0'
 
 end
